f4bc57daa1ba677cb049f33da88e8e89251b675e
CUL Web Team <ucs-webteam@city.ac.uk>
2019-04-03 13:11:23 +0000

    04/03/19 13:11:18 | Merge branch 'master' into feature/course_cards

--- f-course_cards/js/main.js
+++ f-course_cards/js/main.js
@@ -3763,6 +3763,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+ //import backToTopLink from './patterns/back-to-top-link/back-to-top-link';
 
 /* harmony default export */ __webpack_exports__["default"] = ([_patterns_accordion_accordion__WEBPACK_IMPORTED_MODULE_0__["default"], _patterns_cms_editor_warning_cms_editor_warning__WEBPACK_IMPORTED_MODULE_1__["default"], _patterns_cookie_notice_cookie_notice__WEBPACK_IMPORTED_MODULE_2__["default"], _patterns_feedback_feedback__WEBPACK_IMPORTED_MODULE_3__["default"], _patterns_menu_menu__WEBPACK_IMPORTED_MODULE_4__["default"], _patterns_paginated_list_paginated_list__WEBPACK_IMPORTED_MODULE_5__["default"], _patterns_pagination_pagination__WEBPACK_IMPORTED_MODULE_6__["default"], _patterns_tabs_tabs__WEBPACK_IMPORTED_MODULE_7__["default"], _patterns_theme_switcher_theme_switcher__WEBPACK_IMPORTED_MODULE_8__["default"], _patterns_external_link_finder_external_link_finder__WEBPACK_IMPORTED_MODULE_9__["default"]]);
 
@@ -3815,7 +3816,25 @@ const className = 'accordion',
 
 function setSection(heading, open) {
   heading.dataset.open = open;
+  heading.setAttribute('tabindex', '1');
   heading.firstElementChild.setAttribute(_aria_attributes__WEBPACK_IMPORTED_MODULE_3__["default"].expanded, open);
+  let bodyLinks = heading.nextElementSibling.getElementsByTagName('a');
+
+  if (open) {
+    heading.nextElementSibling.classList.add('active');
+    heading.nextElementSibling.style.maxHeight = parseInt(heading.nextElementSibling.scrollHeight + 40) + 'px';
+
+    for (const bodyLink of bodyLinks) {
+      bodyLink.setAttribute('tabindex', '1');
+    }
+  } else {
+    heading.nextElementSibling.classList.remove('active');
+    heading.nextElementSibling.style.maxHeight = null;
+
+    for (const bodyLink of bodyLinks) {
+      bodyLink.setAttribute('tabindex', '-1');
+    }
+  }
 }
 /**
  * Respond to button clicks - open if closed, close if open.
@@ -3917,7 +3936,9 @@ function launchAccordion(accordion) {
 
     if (locationHash === heading.id) {
       idLinked = true;
-      setSection(heading, true);
+      setSection(heading, true); // FF/Safari bug fix
+
+      heading.nextElementSibling.style.maxHeight = '10000px';
     } else {
       setSection(heading, false);
     }
@@ -3926,7 +3947,9 @@ function launchAccordion(accordion) {
   });
 
   if (defaultOpen && !idLinked) {
-    setSection(headings[0], true);
+    setSection(headings[0], true); // FF/Safari bug fix
+
+    headings[0].nextElementSibling.style.maxHeight = '10000px';
   }
 }
 
@@ -5270,10 +5293,10 @@ function toggleLink(link, selected) {
   link.setAttribute(_aria_attributes__WEBPACK_IMPORTED_MODULE_5__["default"].selected, selected);
 
   if (selected) {
-    link.removeAttribute('tabindex');
+    //link.removeAttribute('tabindex');
     link.setAttribute(_aria_attributes__WEBPACK_IMPORTED_MODULE_5__["default"].current, true);
   } else {
-    link.setAttribute('tabindex', -1);
+    link.setAttribute('tabindex', 0);
     link.removeAttribute(_aria_attributes__WEBPACK_IMPORTED_MODULE_5__["default"].current);
   }
 }
@@ -5408,7 +5431,7 @@ function prepareLinks(linkItems) {
 function preparePanels(panels) {
   panels.forEach(panel => {
     panel.setAttribute('role', 'tabpanel');
-    panel.setAttribute('tabindex', -1);
+    panel.setAttribute('tabindex', 0);
     panel.toggleAttribute('hidden', true);
   });
 }
