5a079b44d7227d296bd1b438821413f58d267e9a
CUL Web Team <ucs-webteam@city.ac.uk>
2019-08-08 14:48:23 +0000

    08/08/19 14:48:13 | removed added comment from finder

--- f-content-hub-updates/js/main.js
+++ f-content-hub-updates/js/main.js
@@ -2793,19 +2793,7 @@ function finder__query(props) {
       backgroundFill[0].classList.remove('background-fill--grey');
       e.target.placeholder = props.config.placeholder;
       e.target.value = '';
-      let suggestedResults = document.querySelector('.finder__query__suggestions'); // let suggestedResultsBtns = suggestedResults.querySelectorAll(
-      //     'button'
-      // );
-      // for (const suggestedResultsBtn of suggestedResultsBtns) {
-      //     suggestedResultsBtn.addEventListener('click', () => {
-      //         console.log(suggestedResultsBtn);
-      //     });
-      // }
-      // console.log(suggestedResultsBtns);
-      // suggestedResults.addEventListener('click', () => {
-      //     console.log('a')
-      // });
-
+      let suggestedResults = document.querySelector('.finder__query__suggestions');
       document.addEventListener('click', function (event) {
         let classList = event.target.classList;
 
