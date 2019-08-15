687cd713247580fe72b5555a2ec834c1a1adad26
CUL Web Team <ucs-webteam@city.ac.uk>
2019-08-15 09:05:21 +0000

    08/15/19 09:05:09 | change event to onMouseup to select a suggestions

--- f-finder-ux-fix/js/main.js
+++ f-finder-ux-fix/js/main.js
@@ -2773,7 +2773,6 @@ function finder__query(props) {
   const [inputId] = Object(react__WEBPACK_IMPORTED_MODULE_4__["useState"])('finder--' + props.query.collection + '--' + Math.random().toString(16).slice(-4)); // boolean to show or hide suggestions
 
   const [showSuggestions, setShowSuggestions] = Object(react__WEBPACK_IMPORTED_MODULE_4__["useState"])(false);
-  const [suggestionFocus, setSuggestionFocus] = Object(react__WEBPACK_IMPORTED_MODULE_4__["useState"])(false);
   Object(react__WEBPACK_IMPORTED_MODULE_4__["useEffect"])(() => {
     setPartialQuery(props.query.query);
   }, [props.updating]);
@@ -2823,14 +2822,15 @@ function finder__query(props) {
   // TODO: probably should be refactored into a separate component
 
   const suggestionsList = suggestions && suggestions.length > 0 && react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("ul", {
-    className: showSuggestions || suggestionFocus ? 'finder__query__suggestions show' : 'finder__query__suggestions hide'
+    className: showSuggestions ? 'finder__query__suggestions show' : 'finder__query__suggestions hide'
   }, [...new Set(suggestions)].slice(0, maximumSuggestions).map(suggestion => react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("li", {
     key: suggestion
   }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("button", {
     type: "button",
-    onBlur: () => setSuggestionFocus(false),
-    onFocus: () => setSuggestionFocus(true),
-    onClick: () => {
+    onBlur: () => setShowSuggestions(false),
+    onFocus: () => setShowSuggestions(true),
+    onMouseDown: () => {
+      setShowSuggestions(true);
       setPartialQuery(suggestion);
       setSuggestions([]);
       focusInput();
