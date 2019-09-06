453f8123e3b15672ed8b1e58b509ba46b4c33841
CUL Web Team <ucs-webteam@city.ac.uk>
2019-09-06 10:32:26 +0000

    09/06/19 10:32:15 | clear query button should still be visible even losing focus

--- f-finder-ux-fix/js/finder.js
+++ f-finder-ux-fix/js/finder.js
@@ -1527,9 +1527,6 @@ function finder__clear(props) {
     type: "button",
     onClick: () => {
       props.clear();
-    },
-    onTouchStart: () => {
-      props.clear();
     }
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
     className: "far fa-fw fa-times icon",
