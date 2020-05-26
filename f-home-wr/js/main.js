524f2a8953102d2794d338256e3facc62d25e726
CUL Web Team <ucs-webteam@city.ac.uk>
2020-05-26 14:37:30 +0000

    05/26/20 14:37:06 | Particle setup cont...

--- f-home-wr/js/main.js
+++ f-home-wr/js/main.js
@@ -1311,12 +1311,12 @@ __webpack_require__.r(__webpack_exports__);
 /* v2.0.0
 /* ----------------------------------------------- */
 //const className = 'content-fade-in';
-const idName = 'particles-js';
 const className = 'home__research__wrap'; //particleAnimation
 //function contentFadeIn(contentFadein) {
+// page top
 
 var pJS = function pJS(tag_id, params) {
-  console.log('particle loaded!');
+  console.log('func pjs run');
   var canvas_el = document.querySelector('#' + tag_id + ' > .particles-js-canvas-el');
   /* particles.js variables with default values */
 
@@ -2703,12 +2703,132 @@ window.particlesJS.load = function (tag_id, path_config_json, callback) {
   };
 
   xhr.send();
-};
+}; //////// app.js//////
 
+
+console.log('func app js run');
+particlesJS('particles-js', {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true,
+  "config_demo": {
+    "hide_card": false,
+    "background_color": "#b61924",
+    "background_image": "",
+    "background_position": "50% 50%",
+    "background_repeat": "no-repeat",
+    "background_size": "cover"
+  }
+}); ////////// init walter
+
+console.log('ini script run');
 /* harmony default export */ __webpack_exports__["default"] = ({
   launchFn: pJS,
   launchQuery: ".".concat(className)
-});
+}); /// page bottom
 
 /***/ }),
 
