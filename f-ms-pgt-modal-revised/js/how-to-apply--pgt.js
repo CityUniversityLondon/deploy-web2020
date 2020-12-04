ccc71fdd81bfbb0907aa2c44e59453bdd8d1545f
CUL Web Team <ucs-webteam@city.ac.uk>
2020-12-04 14:45:54 +0000

    12/04/20 14:44:56 | Top-level: if route is direct to apply, set up linked button

--- f-ms-pgt-modal-revised/js/how-to-apply--pgt.js
+++ f-ms-pgt-modal-revised/js/how-to-apply--pgt.js
@@ -272,7 +272,7 @@ function HowToApply(props) {
   //     }
   // }
 
-  let [selection, setSelection] = Object(react__WEBPACK_IMPORTED_MODULE_2__["useState"])('bbb');
+  let [selection, setSelection] = Object(react__WEBPACK_IMPORTED_MODULE_2__["useState"])();
   let options = document.querySelector('.how-to-apply--pgt--js__options');
   const [question, setQuestion] = Object(react__WEBPACK_IMPORTED_MODULE_2__["useState"])(nextQuestion(preferences, entryPoints)),
         [multipleSubjects, setMultipleSubjects] = Object(react__WEBPACK_IMPORTED_MODULE_2__["useState"])(true),
@@ -282,7 +282,7 @@ function HowToApply(props) {
     className: "how-to-apply--pgt--js__modal__content-wrapper"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("p", null, windowPrompt), selection), selection = /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("ul", {
     className: "how-to-apply--pgt--js__options"
-  }, entryPoints.map((qualification, i) => (qualification['options'][0]['options'][0]['options'][0]['options'].length > 1 ? methods = 'multiple' : methods = 'single', /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("li", {
+  }, entryPoints.map((qualification, i) => qualification['options'][0]['options'][0]['options'][0]['options'].length > 1 ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("li", {
     key: 'qualification' + i
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("button", {
     methods: methods,
@@ -330,7 +330,14 @@ function HowToApply(props) {
       } // End of no multiple subjects routes
 
     }
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("span", null, qualification.header))))))),
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("span", null, qualification.header))) : /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("li", {
+    key: 'qualification' + i
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("a", {
+    href: qualification['options'][0]['options'][0]['options'][0]['options'][0]['options']['apply']
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("span", null, qualification.header), "\xA0", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("span", {
+    className: "far fa-external-link",
+    "aria-label": "(external link)"
+  })))))),
         subjectQuestion = question === 'subject' && /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_2___default.a.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("div", {
     className: "how-to-apply--pgt--js__modal__content-wrapper"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("p", null, "Choose the route you wish to apply for:")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("ul", {
