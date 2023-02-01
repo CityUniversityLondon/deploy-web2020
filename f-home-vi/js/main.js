96c68f6aceebc69100345ce90cfe9c67c3f2e9f7
CUL Web Team <ucs-webteam@city.ac.uk>
2023-02-01 09:31:01 +0000

    02/01/23 09:30:31 | Merge pull request #838 from CityUniversityLondon/feature/search-box

--- f-home-vi/js/main.js
+++ f-home-vi/js/main.js
@@ -148,7 +148,7 @@
 /******/
 /******/
 /******/ 	// add entry module to deferred list
-/******/ 	deferredModules.push([0,"vendor-dependencies","core","react","vendor-direct","axios"]);
+/******/ 	deferredModules.push([0,"vendor-dependencies","core","react","axios","vendor-direct"]);
 /******/ 	// run deferred modules when ready
 /******/ 	return checkDeferredModules();
 /******/ })
