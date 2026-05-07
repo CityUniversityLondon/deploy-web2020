c49292a7265e93fc4cd7a95966e33006eb256ca8
CUL Web Team <ucs-webteam@city.ac.uk>
2026-05-07 11:38:34 +0100

    05/07/26 11:38:25 | refactored and refined dark mode styles

--- f-wr-sticky-nav/js/main.js
+++ f-wr-sticky-nav/js/main.js
@@ -7710,7 +7710,6 @@ __webpack_require__.r(__webpack_exports__);
 
 
 function launch(el) {
-  console.log('Sticky Nav pattern launched v2');
   var scrollActiveLink = '';
   /*
   // Create the observer helper div and appended to the DOM
