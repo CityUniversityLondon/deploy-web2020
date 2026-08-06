b4f8a1e06a0579771f6e97d2868f503b61d8bb62
CUL Web Team <ucs-webteam@city.ac.uk>
2026-08-06 11:25:55 +0100

    08/06/26 11:25:41 | Merge branch 'feature/wr-tabs-v26' into feature/bb-course-page-2026

--- f-bb-course-page-2026/js/main.js
+++ f-bb-course-page-2026/js/main.js
@@ -10288,11 +10288,9 @@ var className = 'tabs',
  */
 
 function accordionize(tabs) {
-  console.log('accordionize executed');
   var wrapper = document.createElement('div'),
       accordion = document.createElement('div');
   var tabVersion = tabs.getAttribute('data-version');
-  console.log('tabVersion:', tabVersion);
   accordion.className = tabVersion === 'v26' ? 'accordion accordion-v26 accordion-v26--light' : 'accordion';
   wrapper.className = 'tabs--accordion';
   accordion.id = "accordion".concat(tabs.dataset.assetid);
